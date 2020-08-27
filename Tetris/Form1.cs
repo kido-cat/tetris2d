6c2b4853e62972b9e68bb6cbb12ed80b05419227
sinhvien <sinhvien>
2020-08-27 15:42:50 +0700

    code

---
+++ Tetris/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    public partial class Form1 : Form
+    {
+        public static int H_Board= 20;
+        public static int W_Board = 15;
+        Shape currentShape;
+        int size;
+        int[,] map = new int[150, 9998];
+        int linesRemoved;
+        int score;
+        int a = 0;
+        int Interval;
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyUp += new KeyEventHandler(keyFunc);
+            Init();
+        }
+
+        public void Init()
+        {
+            size = 25;
+            score = 0;
+            linesRemoved = 0;
+            currentShape = new Shape(9, 0);
+            Interval = 300;
+            label1.Text = "Score: " + score;
+            label2.Text = "Lines: " + linesRemoved;
+            label3.Text = "Level: " + a.ToString();
+            timer1.Interval = Interval;
+            timer1.Tick += new EventHandler(update);
+            timer1.Start();
+            Invalidate();
+        }
+
+        private void keyFunc(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    if (!IsIntersects())
+                    {
+                        ResetArea();
+                        currentShape.RotateShape();
+                        Merge();
+                        Invalidate();
+                    }
+                    break;
+                case Keys.Down:
+                    timer1.Interval = 10;
+                    break;
+                case Keys.Right:
+                    if (!CollideHor(1))
+                    {
+                        ResetArea();
+                        currentShape.MoveRight();
+                        Merge();
+                        Invalidate();
+                    }
+                    break;
+                case Keys.Left:
+                    if (!CollideHor(-1))
+                    {
+                        ResetArea();
+                        currentShape.MoveLeft();
+                        Merge();
+                        Invalidate();
+                    }
+                    break;
+                case Keys.P:
+                    timer1.Stop();
+                    break;
+                case Keys.Space:
+                    timer1.Start();
+                    break;
+            }
+        }
+
+        public void ShowNextShape(Graphics e)
+        {
+            for(int i = 0; i < currentShape.sizeNextMatrix; i++)
+            {
+                for (int j = 0; j < currentShape.sizeNextMatrix; j++)
+                {
+                    if (currentShape.nextMatrix[i, j] == 1)
+                    {
+                        e.FillRectangle(Brushes.Red, new Rectangle(700 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 2)
+                    {
+                        e.FillRectangle(Brushes.Yellow, new Rectangle(700 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 3)
+                    {
+                        e.FillRectangle(Brushes.Green, new Rectangle(700 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 4)
+                    {
+                        e.FillRectangle(Brushes.Blue, new Rectangle(700 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 5)
+                    {
+                        e.FillRectangle(Brushes.Orange, new Rectangle(700 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 6)
+                    {
+                        e.FillRectangle(Brushes.Brown, new Rectangle(700 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 7)
+                    {
+                        e.FillRectangle(Brushes.Chocolate, new Rectangle(700 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 8)
+                    {
+                        e.FillRectangle(Brushes.DarkBlue, new Rectangle(700 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (currentShape.nextMatrix[i, j] == 9)
+                    {
+                        e.FillRectangle(Brushes.Pink, new Rectangle(700 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                }
+            }
+        }
+
+        private void update(object sender, EventArgs e)
+        {
+            ResetArea();
+            if (!Collide())
+            {
+                currentShape.MoveDown();
+            }
+            else
+            {
+                Merge();
+                SliceMap();
+                timer1.Interval = Interval;
+                currentShape.ResetShape(3, 0);
+                if (Collide())
+                {
+                    for (int i = 0; i < H_Board; i++)
+                    {
+                        for (int j = 0; j < W_Board; j++)
+                        {
+                            map[i, j] = 0;
+                        }
+                    }
+                    timer1.Tick -= new EventHandler(update);
+                    timer1.Stop();
+                    Init();
+                }
+            }
+            Merge();
+            Invalidate();
+        }
+
+        public void SliceMap()
+        {
+            int count = 0;
+            int curRemovedLines = 0;
+            for(int i = 0; i < H_Board; i++)
+            {
+                count = 0;
+                for (int j = 0; j < W_Board; j++)
+                {
+                    if (map[i, j] != 0)
+                        count++;
+                }
+                if (count == W_Board)
+                {
+                    curRemovedLines++;
+                    for(int k= i; k >= 1; k--)
+                    {
+                        for (int o = 0; o < W_Board; o++)
+                        {
+                            map[k, o] = map[k-1, o];
+                        }
+                    }
+                }
+            }
+            for(int i = 0; i < curRemovedLines; i++)
+            {
+                score += 10 * (i+1);
+                Interval -= 10;
+            }
+            linesRemoved += curRemovedLines;
+
+            a = score / 20;
+            label1.Text = "Score: " + score;
+            label2.Text = "Lines: " + linesRemoved;
+            label3.Text = "Level: " + a;
+        }
+
+        public bool IsIntersects()
+        {
+            for (int i = currentShape.y; i < currentShape.y + currentShape.sizeMatrix; i++)
+            {
+                for (int j = currentShape.x; j < currentShape.x + currentShape.sizeMatrix; j++)
+                {
+                    if (j >= 0 && j <= W_Board -1)
+                    {
+                        if (map[i, j] != 0 && currentShape.matrix[i - currentShape.y, j - currentShape.x] == 0)
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void Merge()
+        {
+            for(int i=currentShape.y;i< currentShape.y + currentShape.sizeMatrix;i++)
+            {
+                for (int j = currentShape.x; j < currentShape.x + currentShape.sizeMatrix; j++)
+                {
+                    if (currentShape.matrix[i - currentShape.y, j - currentShape.x]!=0)
+                    map[i, j] = currentShape.matrix[i - currentShape.y, j - currentShape.x];
+                }
+            }
+        }
+
+        public bool Collide()
+        {
+            for (int i = currentShape.y + currentShape.sizeMatrix - 1; i >= currentShape.y; i--)
+            {
+                for (int j = currentShape.x; j < currentShape.x + currentShape.sizeMatrix; j++)
+                {
+                    if (currentShape.matrix[i - currentShape.y, j - currentShape.x] != 0)
+                    {
+                        if (i + 1 == H_Board)
+                            return true;
+                        if (map[i + 1, j] != 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool CollideHor(int dir)
+        {
+            for (int i = currentShape.y; i < currentShape.y + currentShape.sizeMatrix; i++)
+            {
+                for (int j = currentShape.x; j < currentShape.x + currentShape.sizeMatrix; j++)
+                {
+                    if (currentShape.matrix[i - currentShape.y, j - currentShape.x] != 0)
+                    {
+                        if (j + 1 * dir > W_Board -1 || j + 1 * dir < 0)
+                            return true;
+
+                        if (map[i, j + 1 * dir] != 0)
+                        {
+                            if (j - currentShape.x + 1 * dir >= currentShape.sizeMatrix || j - currentShape.x + 1 * dir < 0)
+                            {
+                                return true;
+                            }
+                            if (currentShape.matrix[i - currentShape.y, j - currentShape.x + 1 * dir] == 0)
+                                return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void ResetArea()
+        {
+            for (int i = currentShape.y; i < currentShape.y + currentShape.sizeMatrix; i++)
+            {
+                for (int j = currentShape.x; j < currentShape.x + currentShape.sizeMatrix; j++)
+                {
+                    if (i >= 0 && j >= 0 && i < H_Board && j < W_Board)
+                    {
+                        if (currentShape.matrix[i - currentShape.y, j - currentShape.x] != 0)
+                        {
+                            map[i, j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void DrawMap(Graphics e)
+        {
+            for(int i = 0; i < H_Board; i++)
+            {
+                for (int j = 0; j < W_Board; j++)
+                {
+                    if (map[i, j] == 1)
+                    {
+                        e.FillRectangle(Brushes.Red, new Rectangle(50 + j * (size)+1, 50 + i * (size)+1, size-1, size-1));
+                    }
+                    if (map[i, j] == 2)
+                    {
+                        e.FillRectangle(Brushes.Yellow, new Rectangle(50 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 3)
+                    {
+                        e.FillRectangle(Brushes.Green, new Rectangle(50 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 4)
+                    {
+                        e.FillRectangle(Brushes.Blue, new Rectangle(50 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 5)
+                    {
+                        e.FillRectangle(Brushes.Orange, new Rectangle(50 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 6)
+                    {
+                        e.FillRectangle(Brushes.Brown, new Rectangle(50 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 7)
+                    {
+                        e.FillRectangle(Brushes.Chocolate, new Rectangle(50 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 8)
+                    {
+                        e.FillRectangle(Brushes.DarkGray, new Rectangle(50 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+                    if (map[i, j] == 9)
+                    {
+                        e.FillRectangle(Brushes.Pink, new Rectangle(50 + j * (size) + 1, 50 + i * (size) + 1, size - 1, size - 1));
+                    }
+
+                }
+            }
+        }
+
+        public void DrawGrid(Graphics g)
+        {
+            for (int i = 0; i <= H_Board; i++)
+            {
+                g.DrawLine(Pens.Black, new Point(50, 50 + i * size), new Point(50 + W_Board * size, 50 + i * size));
+            }
+            for (int i = 0; i <= W_Board; i++)
+            {
+                g.DrawLine(Pens.Black, new Point(50 + i * size, 50), new Point(50 + i * size, 50 + H_Board * size));
+            }
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            DrawGrid(e.Graphics);
+            DrawMap(e.Graphics);
+            ShowNextShape(e.Graphics);
+        }
+    }
+}
